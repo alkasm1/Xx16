95a7bebd801ca1e354927556370e748d24df6772
رأفت عمر <shamwebs1@gmail.com>
2026-04-21 10:09:51 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const cacheName = "xx16-v1"; // غيّر رقم النسخة عند كل تحديث
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== cacheName)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
